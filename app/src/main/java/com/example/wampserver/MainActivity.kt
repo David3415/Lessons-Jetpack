ff497cd49dcb6c05f1b328d7245f8ecc4b8504d9
David <Dgagiev@gmail.com>
2024-07-05 20:18:07 +0300

    Урок #2: Android приложения на Jetpack Compose и KOTLIN #2

--- app/src/main/java/com/example/wampserver/MainActivity.kt
+++ app/src/main/java/com/example/wampserver/MainActivity.kt
@@ -3,15 +3,42 @@ package com.example.wampserver
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent() {
-            newText()
+            Row(modifier = Modifier
+                .background(color= Color.Red).fillMaxSize(0.5f),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(text = "Hello")
+                Text(text = "Hello")
+                Text(text = "Hello")
+                Column(modifier = Modifier
+                    .background(color= Color.Yellow)
+                    .fillMaxSize(0.5f),
+                verticalArrangement = Arrangement.Top
+                ) {
+                Text(text = "Hello")
+                Text(text = "Hello")
+                Text(text = "Hello")
+            }
+            }
+
+
         }
     }
 }
