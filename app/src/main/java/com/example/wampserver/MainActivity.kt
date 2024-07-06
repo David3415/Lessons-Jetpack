28a55c3f3f807b25abfeb83245712c6a25d4521d
David <Dgagiev@gmail.com>
2024-07-06 08:48:18 +0300

    Урок #3: Jetpack Compose – Card, Box, Image

--- app/src/main/java/com/example/wampserver/MainActivity.kt
+++ app/src/main/java/com/example/wampserver/MainActivity.kt
@@ -3,47 +3,74 @@ package com.example.wampserver
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+
+
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent() {
-            Row(modifier = Modifier
-                .background(color= Color.Red).fillMaxSize(0.5f),
-                horizontalArrangement = Arrangement.Start
-            ) {
-                Text(text = "Hello")
-                Text(text = "Hello")
-                Text(text = "Hello")
-                Column(modifier = Modifier
-                    .background(color= Color.Yellow)
-                    .fillMaxSize(0.5f),
-                verticalArrangement = Arrangement.Top
-                ) {
-                Text(text = "Hello")
-                Text(text = "Hello")
-                Text(text = "Hello")
+            Column {
+                ListItem()
+                ListItem()
+                ListItem()
             }
-            }
-
 
         }
     }
 }
-@Preview(showBackground = true)
+
 @Composable
-fun newText(){
-    Text(text = "Hello")
+private fun ListItem() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+         elevation =  CardDefaults.cardElevation(5.dp),
+
+        shape = RoundedCornerShape(15.dp)
+
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentAlignment = Alignment.TopStart
+        ) {
+            Row(){
+               Image(  painter = painterResource(id = R.drawable.i1), contentDescription = "img",
+                   contentScale = ContentScale.Crop,
+                   )
+                Column {
+                    Text(text = "!!!!!!")
+                    Text(text = "!!!!!!")
+                }
+
+            }
+
+        }
+    }
 }
