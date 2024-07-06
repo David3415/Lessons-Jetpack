2ac1f8139c647be1c7d2f34529dea4f2681850d1
David <Dgagiev@gmail.com>
2024-07-06 14:11:34 +0300

    Урок #6: LazyColumn - Items

--- app/src/main/java/com/example/wampserver/MainActivity.kt
+++ app/src/main/java/com/example/wampserver/MainActivity.kt
@@ -4,19 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -40,37 +31,29 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent() {
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
-                ListItem()
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(count = 100) {
+                    Text(
+                        text = "Item $it",
+                        fontSize = 30.sp,
+                        modifier = Modifier.padding(vertical = 10.dp)
+                    )
+                }
             }
-
         }
     }
 }
 
+/*
 @Composable
 private fun ListItem() {
     var counter = remember {
@@ -97,9 +80,11 @@ private fun ListItem() {
 
 
                 .pointerInput(Unit) {
-                    /* detectDragGesturesAfterLongPress { change, dragAmount ->
+                    */
+/* detectDragGesturesAfterLongPress { change, dragAmount ->
                          Log.d("MyLog", "Drag Amount: $dragAmount")
-                     }*/
+                     }*//*
+
                     detectTapGestures {
                         Log.d("MyLog", "Drag : $it")
                         when (++counter.value) {
@@ -130,4 +115,4 @@ private fun ListItem() {
 
         }
     }
-}
+}*/
